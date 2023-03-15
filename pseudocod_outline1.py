1a6d91a9779e70ff7f30120a698b11b4e8c60642
Ariana C. Cason <122395087+arianacason@users.noreply.github.com>
2023-03-15 19:18:10 -0400

    Update pseudocod_outline1.py

--- pseudocod_outline1.py
+++ pseudocod_outline1.py
@@ -1,51 +1,47 @@
-# Import required modules and libraries.
-import snow_api_module
+# 1. Import required libraries
+from flask import Flask, render_template, request
 import os
 
-# Create a Flask app instance.
+# 2. Initialize Flask app
 app = Flask(__name__)
 
-# Define a function to retrieve the hostname of the server via user input.
-def get_hostname():
-    hostname = request.form['hostname']
-    return hostname
+# 3. Define a function to interact with ServiceNow CLI and retrieve hostname and IP address
+def get_snow_info(hostname, os_type):
+    # Retrieve hostname and IP address from ServiceNow using SNOW CLI
+    # Adapt this command to your specific use case
+    # Use the appropriate command for your environment
+    snow_cli_command = f"snow find {hostname}"
+    snow_output = os.popen(snow_cli_command).read()
+    
+    # Parse the output to extract hostname and IP address
+    # This depends on the output format of your SNOW CLI command
+    # (use regex or string manipulation to extract the required data)
+    
+    return hostname, ip_address
 
-# Define a function to retrieve the type of operating system user wants installed or upgraded via user input.
-def get_os():
-    os_type = request.form['os_type']
-    return os_type
-
-# Define a function to retrieve the hostname and IP address from SNOW.
-def get_host_details(hostname):
-    host_details = snow_api_module.get_host_details(hostname)
-    return host_details
-
-# Define a function to install or upgrade the operating system on the specified server.
-def install_os(hostname, os_type):
-    os.system(f'ssh {hostname} "sudo apt-get install {os_type}"')
-
-# Define a route to the homepage of the app.
-@app.route('/')
-def home():
-    return 'Welcome to the OS installer/upgrader app!'
-
-# Define a route to the form for retrieving user input.
-@app.route('/install', methods=['GET', 'POST'])
-def install_form():
+# 4. Define the route for the main page (index) with a form for user input
+@app.route('/', methods=['GET', 'POST'])
+def index():
     if request.method == 'POST':
-        # Get hostname and OS from form data
-        hostname = get_hostname()
-        os_type = get_os()
-        # Get host details from SNOW
-        host_details = get_host_details(hostname)
-        # Install or upgrade OS on the specified server
-        install_os(hostname, os_type)
-        # Return success message
-        return f'OS installation/upgradation on {hostname} successful!'
+        # Get user inputs from the form
+        hostname = request.form['hostname']
+        os_type = request.form['os_type']
+        
+        # Call get_snow_info function to retrieve the hostname and IP address
+        hostname, ip_address = get_snow_info(hostname, os_type)
+        
+        # Redirect to the result page with the retrieved information
+        return render_template('result.html', hostname=hostname, ip_address=ip_address)
     else:
-        # Render the install form template
-        return render_template('install_form.html')
+        # Render the main page with the form for user input
+        return render_template('index.html')
 
-# Run the app.
+# 5. Define the route for the result page to display the retrieved information
+@app.route('/result')
+def result():
+    # Render the result page with the retrieved hostname and IP address
+    return render_template('result.html')
+
+# Run the Flask app
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
